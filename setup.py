49d45875506cc773f422687e8752801a1e47fb36
Pauli Virtanen <pav@iki.fi>
2015-10-30 01:18:10 +0200

    Fix up fake setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import sys
 import subprocess
 
-if sys.argv[1:] == ['install']:
+if sys.argv[1:2] == ['install']:
     subprocess.check_call([sys.executable, 'build.py'],
                           cwd=os.path.abspath(os.path.dirname(__file__)))
 else:
